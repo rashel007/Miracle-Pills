38bf8c65ae0eddc49376625cb8ab79d078c9822e
EA Rashel <rashel320@gmail.com>
2016-12-24 22:58:36 +0600

    connect uipicker with button

--- MagicPills/ViewController.swift
+++ MagicPills/ViewController.swift
@@ -8,11 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    @IBOutlet var btnState: UIButton!
+    
+    @IBOutlet var pickerState: UIPickerView!
+    
+    let states = ["Dhaka", "Rajshahi", "Pabna", "Chittagong", "Barisal"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        pickerState.dataSource = self
+        pickerState.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func btnStatePressed(_ sender: AnyObject) {
+        pickerState.isHidden = false;
+        
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return states.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return states[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        btnState.setTitle(states[row], for: UIControlState())
+    }
 
 }
 
